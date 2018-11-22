100f17454e40e9f0873c2775c1af8d3e6fb1a370
14143 <aio1311hna@gmail.com>
2018-11-22 11:28:30 +0900

    下移動

--- SCR2-9/Takashi.java
+++ SCR2-9/Takashi.java
@@ -15,5 +15,9 @@ public class Takashi extends Actor
     public void act() 
     {
         // Add your action code here.
+        if( Greenfoot.isKeyDown( "down" )){
+            setRotation(90);
+            move(4);
+        }
     }    
 }
