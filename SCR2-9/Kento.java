f67c558c26ef656d9916bd0834dc6e55fb64795f
14198 <3c18ty24@gmail.com>
2018-11-22 10:50:20 +0900

    Kento追加

---
+++ SCR2-9/Kento.java
@@ -0,0 +1,19 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class Kento here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Kento extends Actor
+{
+    /**
+     * Act - do whatever the Kento wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
